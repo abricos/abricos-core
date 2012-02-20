45d2bba08ef254892c2300bc3febe81c4e123c45
Alexander Kuzmin <roosit@abricos.org>
2012-02-20 18:40:22 +0000

    Ядро: мелкие правки

--- modules/sys/js/langs/cp_ru.js
+++ modules/sys/js/langs/cp_ru.js
@@ -1,4 +1,12 @@
 Brick.util.Language.add('ru',{
+	'mod':{
+		'sys': {
+			'title': 'Ядро',
+			'roles':{
+				'50': 'Администрирование'
+			}
+		}
+	},
 	'sys':{
 		'title': 'Настройка сайта',
 		'brick': {
