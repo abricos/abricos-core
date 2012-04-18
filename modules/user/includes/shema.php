9a0e2cc845da86a11a724c576797d5bb407c81ab
Alexander Kuzmin <roosit@abricos.org>
2012-04-18 15:41:32 +0000

    Ядро: при инсталляции, убран е-мейл админа

--- modules/user/includes/shema.php
+++ modules/user/includes/shema.php
@@ -72,7 +72,7 @@ if ($updateManager->isInstall()){
 	// добавление в таблицу администратора
 	$db->query_write("
 		INSERT INTO `".$pfx."user` (`usergroupid`, `username`, `password`, `email`, `joindate`, `salt`) VALUES
-		(6, 'admin', '3f5726cdbe88eac915ffb9e981b72682', 'admin@example.com', ".TIMENOW.", '( R');
+		(6, 'admin', '3f5726cdbe88eac915ffb9e981b72682', '', ".TIMENOW.", '( R');
 	");
 	
 }
