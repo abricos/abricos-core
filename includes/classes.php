4df29e23e474236ed452956a7145e9c6587a13ba
Alexander Kuzmin <roosit@abricos.org>
2013-05-25 11:33:42 +0400

    Добавлен базовый класс списка элементов

---
+++ includes/classes.php
@@ -0,0 +1,97 @@
+<?php 
+/**
+ * @package Abricos
+ * @subpackage Core
+ * @link http://abricos.org
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * @author Alexander Kuzmin <roosit@abricos.org>
+ */
+
+class AbricosItem {
+	public $id;
+
+	public function __construct($d){
+		$this->id = intval($d['id']);
+	}
+
+	public function ToAJAX(){
+		$ret = new stdClass();
+		$ret->id = $this->id;
+		return $ret;
+	}
+}
+
+class AbricosList {
+
+	protected $_list = array();
+	protected $_map = array();
+	protected $_ids = array();
+
+	protected $isCheckDouble = false;
+
+	public function __construct(){
+		$this->_list = array();
+		$this->_map = array();
+	}
+
+	public function Add(AbricosItem $item = null){
+		if (empty($item)){
+			return;
+		}
+
+		if ($this->isCheckDouble){
+			$checkItem = $this->Get($item->id);
+			if (!empty($checkItem)){
+				return;
+			}
+		}
+
+		$index = count($this->_list);
+		$this->_list[$index] = $item;
+		$this->_map[$item->id] = $index;
+
+		array_push($this->_ids, $item->id);
+	}
+
+	/**
+	 * Массив идентификаторов
+	 */
+	public function Ids(){
+		return $this->_ids;
+	}
+
+	public function Count(){
+		return count($this->_list);
+	}
+
+	/**
+	 * @param integer $index
+	 * @return AbricosItem
+	 */
+	public function GetByIndex($index){
+		return $this->_list[$index];
+	}
+
+	/**
+	 * @param integer $id
+	 * @return AbricosItem
+	 */
+	public function Get($id){
+		$index = $this->_map[$id];
+		return $this->_list[$index];
+	}
+
+	public function ToAJAX(){
+		$list = array();
+		$count = $this->Count();
+		for ($i=0; $i<$count; $i++){
+			array_push($list, $this->GetByIndex($i)->ToAJAX());
+		}
+
+		$ret = new stdClass();
+		$ret->list = $list;
+
+		return $ret;
+	}
+}
+?>
