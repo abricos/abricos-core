20255514c4693518b8063b360af1071477b02f4f
Alexander Kuzmin <roosit@abricos.org>
2014-01-19 14:13:55 +0400

    deploy abricos core and modules

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -20,6 +20,7 @@ var DEPLOYOPTDEF = {
 var command = require('command');
 var path = require('path');
 var async = require('async');
+var fs = require('fs-extra');
 var _ = require('lodash');
 
 module.exports = function(grunt) {
@@ -31,6 +32,9 @@ module.exports = function(grunt) {
             function(mainCallback) {
                 exports._setGruntConfig(deployName, mainCallback);
             },
+            function(mainCallback) {
+                exports._deployAbricosCore(deployName, mainCallback);
+            },            
             function(mainCallback) {
                 exports._initDependencies(deployName, mainCallback);
             }],
@@ -44,14 +48,28 @@ module.exports = function(grunt) {
                 }
         );
     });
-    
-    exports._getDeployJSONDir = function(deployName){
+
+    exports._getDeployJSONDir = function(deployName) {
         return path.join(ROOT, "deploy", deployName);
     };
 
-    exports._getDeployJSONFile = function(deployName){
-        var deployJSONDir = exports._getDeployJSONDir(deployName);
-        return path.join(deployJSONDir, "deploy.json");
+    exports._getDeployJSONFile = function(deployName) {
+        var dir = exports._getDeployJSONDir(deployName);
+        return path.join(dir, "deploy.json");
+    };
+
+    exports._getDeploySrcDir = function(deployName) {
+        var dir = exports._getDeployJSONDir(deployName);
+        var options = grunt.config([TASK.name, deployName]) || {};
+
+        return path.join(dir, options.src);
+    };
+
+    exports._getDeployDestDir = function(deployName) {
+        var dir = exports._getDeployJSONDir(deployName);
+        var options = grunt.config([TASK.name, deployName]) || {};
+
+        return path.join(dir, options.dest);
     };
 
     exports._setGruntConfig = function(deployName, mainCallback) {
@@ -77,6 +95,22 @@ module.exports = function(grunt) {
         mainCallback();
     };
 
+    exports._deployAbricosCore = function(deployName, mainCallback) {
+        grunt.log.ok('Deploy Abricos core');
+
+        var src = path.join(ROOT, 'build');
+        var dest = exports._getDeployDestDir(deployName);
+
+        fs.copy(src, dest, function(err) {
+            if (err) {
+                mainCallback(err);
+            }
+            else {
+                mainCallback();
+            }
+        });
+    };
+
     exports._initDependencies = function(deployName, mainCallback) {
 
         var deployJSONDir = exports._getDeployJSONDir(deployName);
@@ -94,24 +128,18 @@ module.exports = function(grunt) {
                 update: true,
                 name: dependName
             }, depend);
-            
+
             // Absolute directory for dependency project
             depend._folder = path.join(deployComponentsDir, dependName);
 
+            depend.deployName = deployName;
+            depend.dependName = dependName;
+
             // Wraps in a closure to hold dependency reference
             (function(depend) {
-                if (grunt.file.exists(depend._folder)) {
-                    if (depend.update) {
-                        stack.push(function(depCallback) {
-                            exports._updateDependency(depend, depCallback);
-                        });
-                    }
-                }
-                else {
-                    stack.push(function(depCallback) {
-                        exports._cloneDependency(depend, depCallback);
-                    });
-                }
+                stack.push(function(depCallback) {
+                    exports._deployDependency(depend, depCallback);
+                });
             })(depend);
         }
 
@@ -120,17 +148,23 @@ module.exports = function(grunt) {
         });
     };
 
-    exports._updateDependency = function(dependency, depCallback) {
-        grunt.log.ok('Updating: ' + dependency.repo +
-                ' [' + dependency.version + ']');
+    exports._updateDependency = function(depend, depCallback) {
+
+        if (!depend.update) {
+            depCallback();
+            return;
+        }
+
+        grunt.log.ok('Updating: ' + depend.repo +
+                ' [' + depend.version + ']');
 
         async.series([
             function(mainCallback) {
                 command.open(ROOT)
                         .on('stdout', command.writeTo(process.stdout))
                         .on('stderr', command.writeTo(process.stderr))
-                        .exec('git', ['fetch', dependency.repo, dependency.version, '--progress'], {
-                            cwd: dependency._folder
+                        .exec('git', ['fetch', depend.repo, depend.version, '--progress'], {
+                            cwd: depend._folder
                         })
                         .then(function() {
                             mainCallback();
@@ -140,8 +174,8 @@ module.exports = function(grunt) {
                 command.open(ROOT)
                         .on('stdout', command.writeTo(process.stdout))
                         .on('stderr', command.writeTo(process.stderr))
-                        .exec('git', ['checkout', dependency.version, '-f'], {
-                            cwd: dependency._folder
+                        .exec('git', ['checkout', depend.version, '-f'], {
+                            cwd: depend._folder
                         })
                         .then(function() {
                             mainCallback();
@@ -151,8 +185,8 @@ module.exports = function(grunt) {
                 command.open(ROOT)
                         .on('stdout', command.writeTo(process.stdout))
                         .on('stderr', command.writeTo(process.stderr))
-                        .exec('git', ['pull', '--rebase', dependency.repo, dependency.version, '--progress'], {
-                            cwd: dependency._folder
+                        .exec('git', ['pull', '--rebase', depend.repo, depend.version, '--progress'], {
+                            cwd: depend._folder
                         })
                         .then(function() {
                             mainCallback();
@@ -164,18 +198,82 @@ module.exports = function(grunt) {
         );
     };
 
-    exports._cloneDependency = function(dependency, depCallback) {
-        grunt.log.ok('Cloning: ' + dependency.repo +
-                ' [' + dependency.version + ']');
+    exports._cloneDependency = function(depend, depCallback) {
+        grunt.log.ok('Cloning: ' + depend.repo +
+                ' [' + depend.version + ']');
 
         command.open(ROOT)
                 .on('stdout', command.writeTo(process.stdout))
                 .on('stderr', command.writeTo(process.stderr))
-                .exec('git', ['clone', dependency.repo, '-b', dependency.version, dependency._folder, '--progress'], {
+                .exec('git', ['clone', depend.repo, '-b', depend.version, depend._folder, '--progress'], {
                     cwd: ROOT
                 })
                 .then(function() {
                     depCallback();
                 });
+    };
+
+    exports._buildAbricosModuleDependency = function(depend, mainCallback) {
+        grunt.log.ok('Build Abricos module: ' + depend.dependName);
+
+        var cwd = depend._folder;
+        var src = path.join(cwd, 'src');
+        var dest = path.join(cwd, 'build', 'modules', depend.name);
+
+        fs.copy(src, dest, function(err) {
+            if (err) {
+                mainCallback(err);
+            }
+            else {
+                mainCallback();
+            }
+        });
+    };
+
+    exports._deployAbricosModuleDependency = function(depend, mainCallback) {
+        grunt.log.ok('Deploy Abricos module: ' + depend.dependName);
+
+        var src = path.join(depend._folder, 'build');
+        var dest = exports._getDeployDestDir(depend.deployName);
+
+        fs.copy(src, dest, function(err) {
+            if (err) {
+                mainCallback(err);
+            }
+            else {
+                mainCallback();
+            }
+        });
+    };
+
+    exports._deployDependency = function(depend, deployCallback) {
+        var stack = [];
+
+        stack.push(function(depCallback) {
+            if (grunt.file.exists(depend._folder)) {
+                exports._updateDependency(depend, depCallback);
+            } else {
+                exports._cloneDependency(depend, depCallback);
+            }
+        });
+
+        switch (depend.type) {
+            case "module":
+                stack.push(function(depCallback) {
+                    exports._buildAbricosModuleDependency(depend, depCallback);
+                });
+                stack.push(function(depCallback) {
+                    exports._deployAbricosModuleDependency(depend, depCallback);
+                });
+                break;
+            case "template":
+                break;
+        }
+
+        async.series(stack,
+                function() {
+                    deployCallback();
+                }
+        );
     };
 };
