253a9b985f645b9b2ac3af3bd7344422eb61aece
Alexander Kuzmin <roosit@abricos.org>
2014-10-29 14:18:04 +0300

    add UserListConfig class

--- src/includes/classes.php
+++ src/includes/classes.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * @package Abricos
  * @subpackage Core
@@ -6,92 +7,139 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
  * @author Alexander Kuzmin <roosit@abricos.org>
  */
-
 class AbricosItem {
-	public $id;
+    public $id;
 
-	public function __construct($d){
-		$this->id = intval($d['id']);
-	}
+    public function __construct($d) {
+        $this->id = intval($d['id']);
+    }
 
-	public function ToAJAX(){
-		$ret = new stdClass();
-		$ret->id = $this->id;
-		return $ret;
-	}
+    public function ToAJAX() {
+        $ret = new stdClass();
+        $ret->id = $this->id;
+        return $ret;
+    }
 }
 
 class AbricosList {
 
-	protected $_list = array();
-	protected $_map = array();
-	protected $_ids = array();
+    /**
+     * @var AbricosListConfig
+     */
+    public $config;
 
-	protected $isCheckDouble = false;
+    public $classConfig = AbricosListConfig;
 
-	public function __construct(){
-		$this->_list = array();
-		$this->_map = array();
-	}
+    protected $_list = array();
+    protected $_map = array();
+    protected $_ids = array();
 
-	public function Add($item){
-		if (empty($item)){
-			return;
-		}
+    protected $isCheckDouble = false;
 
-		if ($this->isCheckDouble){
-			$checkItem = $this->Get($item->id);
-			if (!empty($checkItem)){
-				return;
-			}
-		}
+    public function __construct($config = null) {
+        $this->_list = array();
+        $this->_map = array();
+        if (empty($config)) {
+            $config = new $this->classConfig();
+        }
+        $this->config = $config;
+    }
 
-		$index = count($this->_list);
-		$this->_list[$index] = $item;
-		$this->_map[$item->id] = $index;
+    public function Add($item) {
+        if (empty($item)) {
+            return;
+        }
 
-		array_push($this->_ids, $item->id);
-	}
+        if ($this->isCheckDouble) {
+            $checkItem = $this->Get($item->id);
+            if (!empty($checkItem)) {
+                return;
+            }
+        }
 
-	/**
-	 * Массив идентификаторов
-	 */
-	public function Ids(){
-		return $this->_ids;
-	}
+        $index = count($this->_list);
+        $this->_list[$index] = $item;
+        $this->_map[$item->id] = $index;
 
-	public function Count(){
-		return count($this->_list);
-	}
+        array_push($this->_ids, $item->id);
+    }
 
-	/**
-	 * @param integer $index
-	 * @return AbricosItem
-	 */
-	public function GetByIndex($index){
-		return $this->_list[$index];
-	}
+    /**
+     * Массив идентификаторов
+     */
+    public function Ids() {
+        return $this->_ids;
+    }
 
-	/**
-	 * @param integer $id
-	 * @return AbricosItem
-	 */
-	public function Get($id){
-		$index = $this->_map[$id];
-		return $this->_list[$index];
-	}
+    public function Count() {
+        return count($this->_list);
+    }
 
-	public function ToAJAX(){
-		$list = array();
-		$count = $this->Count();
-		for ($i=0; $i<$count; $i++){
-			array_push($list, $this->GetByIndex($i)->ToAJAX());
-		}
+    /**
+     * @param integer $index
+     * @return AbricosItem
+     */
+    public function GetByIndex($index) {
+        return $this->_list[$index];
+    }
 
-		$ret = new stdClass();
-		$ret->list = $list;
+    /**
+     * @param integer $id
+     * @return AbricosItem
+     */
+    public function Get($id) {
+        $index = $this->_map[$id];
+        return $this->_list[$index];
+    }
 
-		return $ret;
-	}
+    public function ToAJAX() {
+        $list = array();
+        $count = $this->Count();
+        for ($i = 0; $i < $count; $i++) {
+            array_push($list, $this->GetByIndex($i)->ToAJAX());
+        }
+
+        $ret = new stdClass();
+        $ret->list = $list;
+        $ret->config = $this->config->ToAJAX();
+
+        return $ret;
+    }
 }
+
+class AbricosListConfig {
+    public $page = 1;
+    public $limit = 0;
+
+    private $_total = 0;
+
+    public function __construct($d = null) {
+        if (!is_array($d)) {
+            return;
+        }
+        $this->page = max(intval($d['page']), 1);
+        $this->limit = intval($d['limit']);
+    }
+
+    public function SetTotal($total){
+        $this->_total = intval($total);
+    }
+
+    public function GetTotal(){
+        return $this->_total;
+    }
+
+    public function ToAJAX() {
+        $ret = new stdClass();
+        $ret->page = $this->page;
+        $ret->limit = $this->limit;
+        $ret->total = $this->_total;
+        return $ret;
+    }
+
+    public function GetFrom() {
+        return ($this->page - 1) * $this->limit;
+    }
+}
+
 ?>
