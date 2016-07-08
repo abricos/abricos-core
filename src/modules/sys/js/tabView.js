c7d8cd41c9c31ff5f5dd9a31ae7e8e6d4b8f60f7
Alexander Kuzmin <roosit@abricos.org>
2016-07-08 15:15:28 +0300

    update: TabViewWidget extended functions

--- src/modules/sys/js/tabView.js
+++ src/modules/sys/js/tabView.js
@@ -8,49 +8,75 @@ Component.entryPoint = function(NS){
     var Y = Brick.YUI,
         COMPONENT = this;
 
-    NS.TabViewPage = Y.Base.create('TabViewPage', Y.Base, [], {}, {
-        ATTRS: {
-            owner: {value: null},
-            name: {
-                getter: function(val){
-                    return Y.Lang.isString(val) && val !== "" ? val : "tab" + this.get('index');
-                },
-            },
-            index: {value: 0},
-            title: {
-                getter: function(val){
-                    return Y.Lang.isString(val) && val !== "" ? val : this.get('name');
-                },
-            },
-            content: {
-                value: "",
-                setter: function(val){
-                    if (!Y.Lang.isString(val)){
-                        val = "";
-                    }
-                    var node = this.get('node');
-                    if (node){
-                        node.setHTML(val);
-                    }
-                    return val;
-                },
-                getter: function(val){
-                    var node = this.get('node');
-                    if (node){
-                        return node.getHTML();
-                    }
-                    return val;
-                }
-            },
-            node: {value: null}
+    var TabViewPageBase = function(){
+    };
+    TabViewPageBase.prototype = {
+        _renderTitle: function(val){
+            var title = val || this.get('title'),
+                headerTitleNode = this.get('headerTitleNode');
+            if (!headerTitleNode){
+                return;
+            }
+            headerTitleNode.setHTML(title);
         }
-    });
+    };
+    TabViewPageBase.ATTRS = {
+        owner: {value: null},
+        headerNode: {value: null},
+        headerTitleNode: {value: null},
+        paneNode: {value: null},
+        srcNode: {value: null},
+        name: {
+            getter: function(val){
+                return Y.Lang.isString(val) && val !== "" ? val : "tab" + this.get('index');
+            },
+        },
+        index: {value: 0},
+        title: {
+            getter: function(val){
+                return Y.Lang.isString(val) && val !== "" ? val : this.get('name');
+            },
+            setter: function(val){
+                this._renderTitle(val);
+                return val;
+            }
+        },
+        content: {
+            value: "",
+            setter: function(val){
+                if (!Y.Lang.isString(val)){
+                    val = "";
+                }
+                var node = this.get('srcNode');
+                if (node){
+                    node.setHTML(val);
+                }
+                return val;
+            },
+            getter: function(val){
+                var node = this.get('srcNode');
+                if (node){
+                    return node.getHTML();
+                }
+                return val;
+            }
+        },
+    };
+    NS.TabViewPageBase = TabViewPageBase;
+
+    NS.TabViewPage = Y.Base.create('TabViewPage', Y.Base, [
+        NS.TabViewPageBase
+    ], {}, {});
 
     NS.TabViewWidget = Y.Base.create('TabViewWidget', NS.AppWidget, [], {
-        onInitAppWidget: function(err, appInstance){
+        initializer: function(){
             this._list = [];
         },
         destructor: function(){
+            this.each(function(tab){
+                tab.destroy();
+            }, this);
+            this._list = [];
         },
         size: function(){
             return this._list.length;
@@ -59,31 +85,37 @@ Component.entryPoint = function(NS){
             options = Y.merge({
                 name: "",
                 title: "",
-                content: ""
+                content: "",
+                TabViewPage: NS.TabViewPage
             }, options || {});
 
             var tp = this.template,
                 list = this._list,
                 index = list.length,
-                headerHTML = tp.getHTML('headerList'),
-                tab = new NS.TabViewPage(options);
+                headerHTML = tp.replace('header', {index: index}),
+                paneHTML = tp.replace('pane', {index: index}),
+                TabViewPage = options.TabViewPage,
+                contentHTML = options.content;
 
-            headerHTML += tp.replace('header', {
-                index: index,
-                title: tab.get("title")
-            });
-            tp.setHTML('headerList', headerHTML);
+            delete options.content;
+            delete options.TabViewPage;
 
-            var paneHTML = tp.replace('pane', {index: index});
-
+            tp.one('headerList').appendChild(headerHTML);
             tp.one('paneList').appendChild(paneHTML);
 
-            var nodePane = tp.one('pane.index-' + index);
+            options.owner = this;
+            options.index = index;
+            options.headerNode = tp.one('header.index-' + index);
+            options.headerTitleNode = tp.one('header.title-' + index);
+            options.paneNode = tp.one('pane.index-' + index);
+            options.srcNode = tp.one('pane.content-' + index);
 
-            tab.set('index', index);
-            tab.set('owner', this);
-            tab.set('node', nodePane);
-            tab.set('content', options.content);
+            var tab = new TabViewPage(options);
+            tab._renderTitle();
+
+            if (contentHTML !== ""){
+                tab.set('content', contentHTML);
+            }
 
             list[list.length] = tab;
 
@@ -106,18 +138,23 @@ Component.entryPoint = function(NS){
         },
         selectTab: function(index){
             index = index | 0;
-            
-            var tp = this.template;
-
             this.each(function(tab, i){
-                tp.toggleClass('header.index-' + i, 'active', index === i);
-                tp.toggleClass('pane.index-' + i, 'active', index === i);
+                if (index === i){
+                    tab.get('paneNode').addClass('active');
+                    tab.get('headerNode').addClass('active');
+                } else {
+                    tab.get('paneNode').removeClass('active');
+                    tab.get('headerNode').removeClass('active');
+                }
+                // tab.get('paneNode').toggleClass('active', index === i);
+                // tab.get('headerNode').toggleClass('active', index === i);
             }, this);
         },
     }, {
         ATTRS: {
             component: {value: COMPONENT},
             templateBlockName: {value: 'widget,header,pane'},
+            TabViewPage: {value: NS.TabViewPage}
         },
         CLICKS: {
             selectTab: {
