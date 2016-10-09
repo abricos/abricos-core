d1b8198b152017240befa4e79fce2e5ed9f35f76
Alexander Kuzmin <roosit@abricos.org>
2016-10-09 11:29:26 +0300

    cache function in app

--- src/includes/classes.php
+++ src/includes/classes.php
@@ -103,6 +103,51 @@ abstract class AbricosApplication {
         $this->_cache = array();
     }
 
+    public function CacheExists(){
+        $count = func_num_args();
+        $cache = $this->_cache;
+        for ($i = 0; $i < $count; $i++){
+            $arg = func_get_arg($i);
+            if (!isset($cache[$arg])){
+                return false;
+            }
+            $cache = $cache[$arg];
+        }
+
+        return !empty($cache);
+    }
+
+    public final function Cache(){
+        $count = func_num_args();
+        $cache = $this->_cache;
+        for ($i = 0; $i < $count; $i++){
+            $arg = func_get_arg($i);
+            if (!isset($cache[$arg])){
+                return null;
+            }
+            $cache = $cache[$arg];
+        }
+        return $cache;
+    }
+
+    public final function SetCache(){
+        $count = func_num_args();
+        $cache = &$this->_cache;
+        if ($count < 2){
+            throw new Exception('Invalid param in SetCache');
+        }
+
+        for ($i = 0; $i < $count - 1; $i++){
+            $arg = func_get_arg($i);
+            if (!isset($cache[$arg])){
+                $cache[$arg] = array();
+            }
+            $cache = &$cache[$arg];
+        }
+        $cache = func_get_arg($count - 1);
+        return func_get_arg($count - 1);
+    }
+
     protected function GetAppClasses(){
         return array();
     }
