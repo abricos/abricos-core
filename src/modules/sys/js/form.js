a411e9ad8c059e9348e782f185b9464cfadb878a
Alexander Kuzmin <roosit@abricos.org>
2017-01-26 16:39:54 +0300

    workfix

--- src/modules/sys/js/form.js
+++ src/modules/sys/js/form.js
@@ -1,9 +1,3 @@
-/*!
- * Abricos Platform (http://abricos.org)
- * Copyright 2008-2014 Alexander Kuzmin <roosit@abricos.org>
- * Licensed under the MIT license
- */
-
 var Component = new Brick.Component();
 Component.requires = {
     mod: [
